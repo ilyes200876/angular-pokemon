eb6843d618759116e62d788b7e37f8b5a44dd49b
ilyes200876 <ilyes200876@live.fr>
2023-09-07 14:41:05 +0200

    form conditions

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
   ]
 })
 export class ContactComponent {
-  bookTitle: string;
+  
+  username:string;
+  password: string;
+  messageUser: string;
+  messagePwd: string;
+
+  onSubmit(form: NgForm){
+    console.log(form.value);
+    if(form.value.username.length < 2 || form.value.username == ''){
+      // alert("Votre username doit contenir au moins 2 carractères");
+      this.messageUser = "😵";
+    }
+    if(form.value.username.length >= 2){
+      this.messageUser = "👌";
+    }
+    
+    if(form.value.password.length > 6 || form.value.username == ''){
+      // alert("Votre pass<ord doit contenir au max 6 carractères");
+      this.messagePwd = "😵";
+    }
+    if(form.value.password.length <= 6){
+      this.messagePwd = "👌";
+    }
+    
+    form.resetForm();
+    
+  }
+
 }
